2261594fe558bafbc003d88f2d8f3eccfb6cd957
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-07-13 19:30:44 +0300

    Add files via upload

---
+++ 01WorkingWithAbstraction/Lab/L03_StudentSytem/Student.java
@@ -0,0 +1,28 @@
+package OOP.WorkingWithAbstraction_Lab.L03_StudentSytem;
+
+public class Student {
+    private String name;
+    private int age;
+    private double grade;
+
+    public Student(String name, int age, double grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public String getInfo() {
+
+        String studentInfo = String.format("%s is %s years old.", this.name, this.age);
+
+        if (this.grade >= 5.00) {
+            studentInfo += " Excellent student.";
+        } else if (this.grade < 5.00 && this.grade >= 3.50) {
+            studentInfo += " Average student.";
+        } else {
+            studentInfo += " Very nice person.";
+        }
+
+        return studentInfo;
+    }
+}
