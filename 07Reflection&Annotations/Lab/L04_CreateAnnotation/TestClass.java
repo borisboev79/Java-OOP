977417dc11ec315810d0d8cbab53871d992d6e7b
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-07-20 09:02:19 +0300

    Add files via upload

---
+++ 07Reflection&Annotations/Lab/L04_CreateAnnotation/TestClass.java
@@ -0,0 +1,8 @@
+package OOP.ReflectionAndAnnotations_Lab.L04_CreateAnnotation;
+
+import java.lang.annotation.Annotation;
+
+@Subject(categories = {"Test", "Annotations"})
+public class TestClass {
+
+}
