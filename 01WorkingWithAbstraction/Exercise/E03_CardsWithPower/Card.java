b9169830dac911b63c04770f79822017bc442d7b
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-07-20 21:47:43 +0300

    Add files via upload

---
+++ 01WorkingWithAbstraction/Exercise/E03_CardsWithPower/Card.java
@@ -0,0 +1,31 @@
+package OOP.WorkingWithAbstraction_Exercise.E03_CardsWIthPower;
+
+public class Card {
+    private CardSuits cardSuit;
+    private CardRanks cardRank;
+
+    public Card(CardSuits cardSuit, CardRanks cardRank) {
+        this.cardSuit = cardSuit;
+        this.cardRank = cardRank;
+    }
+
+    public CardSuits getCardSuit() {
+        return cardSuit;
+    }
+
+    public void setCardSuit(CardSuits cardSuit) {
+        this.cardSuit = cardSuit;
+    }
+
+    public CardRanks getCardRank() {
+        return cardRank;
+    }
+
+    public void setCardRank(CardRanks cardRank) {
+        this.cardRank = cardRank;
+    }
+
+    public int getPower(CardSuits suit, CardRanks rank) {
+        return suit.getSuitPower() + rank.getRankPower();
+    }
+}
