8659415a89d5762b1ac12f05e8fd9c1c184b51b6
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-07-22 22:40:45 +0300

    Add files via upload

---
+++ 01WorkingWithAbstraction/Exercise/E04_TrafficLights/Main.java
@@ -0,0 +1,33 @@
+package OOP.WorkingWithAbstraction_Exercise.E04_TrafficLights;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> colors = Arrays.stream(scanner.nextLine().split("\\s+")).collect(Collectors.toList());
+
+        List<TrafficLight> trafficLights = new ArrayList<>();
+
+
+        for (String color : colors) {
+            Color currentColor = Color.valueOf(color);
+            TrafficLight currentTrafficLight = new TrafficLight(currentColor);
+            trafficLights.add(currentTrafficLight);
+        }
+
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            for (TrafficLight trafficLight : trafficLights) {
+                trafficLight.changeColor();
+                System.out.print(trafficLight.getCurrentColor() + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
